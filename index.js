eec5b91722871ef9ae872e304eb204674206d56b
Rama <ramiroquesadaperez@gmail.com>
2022-08-27 01:21:46 -0300

    Basic logic of a single game created

--- index.js
+++ index.js
@@ -8,9 +8,55 @@ let randomChoice = Math.floor(Math.random() * opciones.length);
 
 
 let getComputerChoice = ()  => {
-    return opciones[randomChoice];
+    return opciones[randomChoice].toUpperCase();
 }
 
 
 
+let playerSelection = prompt().toUpperCase();
 
+function playRound(playerSelection, computerChoice){
+
+    console.log(playerSelection);
+    console.log(computerChoice);
+    if (playerSelection === computerChoice){
+        console.log ("Tie");
+    }
+
+    else if (playerSelection === "ROCK" && computerChoice === "SCISSORS"){
+        console.log ("You WIN! ROCK beats SCISSORS!");
+    }
+    else if (playerSelection === "ROCK" && computerChoice === "PAPER"){
+        console.log ("You LOSE! PAPER beats ROCK!");
+    }
+
+    else if (playerSelection === "PAPER" && computerChoice === "ROCK"){
+        console.log ("You WIN! PAPER beats ROCK!");
+    }    
+    else if (playerSelection === "PAPER" && computerChoice === "SCISSORS"){
+        console.log ("You LOSE! SCISSORS beats PAPER!")
+    }
+
+
+    else if (playerSelection === "SCISSORS" && computerChoice === "PAPER"){
+        console.log ("You WIN! SCISSORS beats PAPER!");
+    }
+
+    else if (playerSelection === "SCISSORS" && computerChoice === "ROCK"){
+        console.log ("You LOSE! SCISSORS beats ROCK!");
+    }
+
+
+
+}
+
+/*
+playRound(playerSelection, getComputerChoice());
+*/
+
+function game(){
+
+    
+
+
+}
